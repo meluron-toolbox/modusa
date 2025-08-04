8c7c9ef9591a3154b2c652630b20f0e8621e0077
Ankit Anand <ankit0.anand0@gmail.com>
2025-08-04 21:37:28 +0530

    [Update] Update the plotting modules - Now it is easier to plot 1D and 2D signals - Multiple signals can be plotted together - Now if xlim is set, all the axis should be aligned

--- src/modusa/tools/plotter.py
+++ src/modusa/tools/plotter.py
@@ -5,9 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.patches import Rectangle
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 #======== 1D ===========
-def plot1d(y, x, ann=None, events=None, xlim=None, ylim=None, xlabel=None, ylabel=None, title=None, legend=None):
+def plot1d(*args, ann=None, events=None, xlim=None, ylim=None, xlabel=None, ylabel=None, title=None, legend=None, get_ax=False):
 		"""
 		Plots a 1D signal using matplotlib.
 
@@ -24,10 +25,9 @@ def plot1d(y, x, ann=None, events=None, xlim=None, ylim=None, xlabel=None, ylabe
 	
 		Parameters
 		----------
-		y : array-like
+		*args : tuple[array-like, array-like]
 			- The signal values to be plotted.
-		x : array-like
-			- The corresponding x-axis values (e.g., time or sample index).
+			- E.g. (y1, x1), (y2, x2), ...
 		ann : list[tuple[Number, Number, str] | None
 			- A list of annotations to mark specific points. Each tuple should be of the form (start, end, label).
 			- Default: None => No annotation.
@@ -50,44 +50,55 @@ def plot1d(y, x, ann=None, events=None, xlim=None, ylim=None, xlabel=None, ylabe
 			- Title of the plot.
 			- Default: None
 		legend : list[str] | None
-			List of legend labels corresponding to each signal if plotting multiple lines.
+			- List of legend labels corresponding to each signal if plotting multiple lines.
+			- Default: None
 	
 		Returns
 		-------
 		plt.Figure
 			Matplolib figure.
 		"""
-		fig = plt.figure(figsize=(16, 3))
-		gs = gridspec.GridSpec(2, 3, height_ratios=[0.2, 1], width_ratios=[1, 0.1, 0.01])
+	
+		for arg in args:
+			if len(arg) != 2:
+				raise ValueError(f"Data to plot needs to have 2 arrays (y, x)")
+		if isinstance(legend, str): legend = (legend, )
+		
+		if legend is not None:
+			if len(legend) < len(args):
+				raise ValueError(f"Legend should be provided for each signal.")
+
+		fig = plt.figure(figsize=(16, 2))
+		gs = gridspec.GridSpec(2, 1, height_ratios=[0.2, 1])
+			
 		colors = plt.get_cmap('tab10').colors
 		
 		signal_ax = fig.add_subplot(gs[1, 0])
 		annotation_ax = fig.add_subplot(gs[0, 0], sharex=signal_ax)
 		
-		legend_ax = fig.add_subplot(gs[1, 1])
-		colorbar_ax = fig.add_subplot(gs[1, 2])
-		
-		# Making annotation axis spines thicker
-		for spine in annotation_ax.spines.values():
-			spine.set_linewidth(2)
-		
-		# Add xlim
+		# Set lim
 		if xlim is not None:
-			x_start, x_end = xlim
-			signal_ax.set_xlim([x_start, x_end])
+			signal_ax.set_xlim(xlim)
+		
+		if ylim is not None:
+			signal_ax.set_ylim(ylim)
+		
 			
 		# Add signal plot
-		signal_ax.plot(x, y, label=legend)
+		for signal in args:
+			y, x = signal
+			signal_ax.plot(x, y, label=legend)
 		
 		# Add annotations
 		if ann is not None:
+			annotation_ax.set_ylim(0, 1)
 			for i, (start, end, tag) in enumerate(ann):
 				if xlim is not None:
-					if end < x_start or start > x_end:
+					if end < xlim[0] or start > xlim[1]:
 						continue  # Skip out-of-view regions
 					# Clip boundaries to xlim
-					start = max(start, x_start)
-					end = min(end, x_end)
+					start = max(start, xlim[0])
+					end = min(end, xlim[1])
 					
 				color = colors[i % len(colors)]
 				width = end - start
@@ -100,7 +111,7 @@ def plot1d(y, x, ann=None, events=None, xlim=None, ylim=None, xlabel=None, ylabe
 		if events is not None:
 			for xpos in events:
 				if xlim is not None:
-					if x_start <= xpos <= x_end:
+					if xlim[0] <= xpos <= xlim[1]:
 						annotation_ax.axvline(x=xpos, color='black', linestyle='--', linewidth=1.5)
 				else:
 					annotation_ax.axvline(x=xpos, color='black', linestyle='--', linewidth=1.5)
@@ -108,25 +119,36 @@ def plot1d(y, x, ann=None, events=None, xlim=None, ylim=None, xlabel=None, ylabe
 		# Add legend
 		if legend is not None:
 			handles, labels = signal_ax.get_legend_handles_labels()
-			legend_ax.legend(handles, labels, loc='upper left')
+			fig.legend(handles, legend,
+								loc='upper right',
+								bbox_to_anchor=(0.9, 1.2),
+								ncol=len(legend),
+								frameon=False)
 			
 		# Set title, labels
 		if title is not None:
-			annotation_ax.set_title(title, pad=10)
+			annotation_ax.set_title(title, pad=10, size=11)
 		if xlabel is not None:
 			signal_ax.set_xlabel(xlabel)
 		if ylabel is not None:
 			signal_ax.set_ylabel(ylabel)
-			
-		annotation_ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-		legend_ax.axis("off")
-		colorbar_ax.axis("off")
-		plt.tight_layout()
+		
+		# Decorating annotation axis thicker
+		if ann is not None:
+			annotation_ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+		else:
+			annotation_ax.axis("off")
+		
+		
+		fig.subplots_adjust(hspace=0.01, wspace=0.05)
 		plt.close()
-		return fig
+		if get_ax is True:
+			return fig, annotation_ax, signal_ax
+		else:
+			return fig
 
 #======== 2D ===========
-def plot2d(M, y, x, ann=None, events=None, xlim=None, ylim=None, origin="lower", Mlabel=None, xlabel=None, ylabel=None, title=None, legend=None):
+def plot2d(*args, ann=None, events=None, xlim=None, ylim=None, origin="lower", Mlabel=None, xlabel=None, ylabel=None, title=None, legend=None):
 	"""
 	Plots a 2D matrix (e.g., spectrogram or heatmap) with optional annotations and events.
 
@@ -143,12 +165,9 @@ def plot2d(M, y, x, ann=None, events=None, xlim=None, ylim=None, origin="lower",
 
 	Parameters
 	----------
-	M : 2D array-like
-		- The 2D data matrix to visualize (e.g., magnitude or energy over time and frequency).
-	y : array-like
-		- Y-axis values (e.g., frequency bins or channels).
-	x : array-like
-		- X-axis values (e.g., time or sample index).
+	*args : tuple[array-like, array-like]
+		- The signal values to be plotted.
+		- E.g. (M1, y1, x1), (M2, y2, x2), ...
 	ann : list[tuple[Number, Number, str]] | None
 		- A list of annotation spans. Each tuple should be (start, end, label).
 		- Default: None (no annotations).
@@ -185,40 +204,48 @@ def plot2d(M, y, x, ann=None, events=None, xlim=None, ylim=None, origin="lower",
 	matplotlib.figure.Figure
 		The matplotlib Figure object.
 	"""
-	fig = plt.figure(figsize=(16, 5))
-	gs = gridspec.GridSpec(2, 3, height_ratios=[0.1, 1], width_ratios=[1, 0.1, 0.01])
+	
+	for arg in args:
+		if len(arg) != 3:
+			raise ValueError(f"Data to plot needs to have 3 arrays (M, y, x)")
+
+	fig = plt.figure(figsize=(16, 4))
+	gs = gridspec.GridSpec(3, 1, height_ratios=[0.2, 0.1, 1]) # colorbar, annotation, signal
+
 	colors = plt.get_cmap('tab10').colors
 	
-	signal_ax = fig.add_subplot(gs[1, 0])
-	annotation_ax = fig.add_subplot(gs[0, 0], sharex=signal_ax)
+	signal_ax = fig.add_subplot(gs[2, 0])
+	annotation_ax = fig.add_subplot(gs[1, 0], sharex=signal_ax)
 	
-	legend_ax = fig.add_subplot(gs[1, 1])
-	colorbar_ax = fig.add_subplot(gs[1, 2])
+	colorbar_ax = fig.add_subplot(gs[0, 0])
+	colorbar_ax.axis("off")
 	
-	# Making annotation axis spines thicker
-	for spine in annotation_ax.spines.values():
-		spine.set_linewidth(2)
 	
-	# Add xlim
+	# Add lim
 	if xlim is not None:
-		x_start, x_end = xlim
-		signal_ax.set_xlim([x_start, x_end])
+		signal_ax.set_xlim(xlim)
+		
+	if ylim is not None:
+		signal_ax.set_ylim(ylim)
 		
 	# Add signal plot
-	dx = x[1] - x[0]
-	dy = y[1] - y[0]
-	extent=[x[0] - dx/2, x[-1] + dx/2, y[0] - dy/2, y[-1] + dy/2]
-	im = signal_ax.imshow(M, aspect="auto", origin=origin, extent=extent)
+	for signal in args:
+		M, y, x = signal
+		dx = x[1] - x[0]
+		dy = y[1] - y[0]
+		extent=[x[0] - dx/2, x[-1] + dx/2, y[0] - dy/2, y[-1] + dy/2]
+		im = signal_ax.imshow(M, aspect="auto", origin=origin, extent=extent)
 	
 	# Add annotations
 	if ann is not None:
+		annotation_ax.set_ylim(0, 1)
 		for i, (start, end, tag) in enumerate(ann):
 			if xlim is not None:
-				if end < x_start or start > x_end:
+				if end < xlim[0] or start > xlim[1]:
 					continue  # Skip out-of-view regions
 				# Clip boundaries to xlim
-				start = max(start, x_start)
-				end = min(end, x_end)
+				start = max(start, xlim[0])
+				end = min(end, xlim[1])
 				
 			color = colors[i % len(colors)]
 			width = end - start
@@ -231,26 +258,45 @@ def plot2d(M, y, x, ann=None, events=None, xlim=None, ylim=None, origin="lower",
 	if events is not None:
 		for xpos in events:
 			if xlim is not None:
-				if x_start <= xpos <= x_end:
+				if xlim[0] <= xpos <= xlim[1]:
 					annotation_ax.axvline(x=xpos, color='black', linestyle='--', linewidth=1.5)
 			else:
 				annotation_ax.axvline(x=xpos, color='black', linestyle='--', linewidth=1.5)
 				
 	# Add colorbar
-	cbar = plt.colorbar(im, cax=colorbar_ax)
+	# Create an inset axis on top-right of signal_ax
+	cax = inset_axes(
+		colorbar_ax,
+		width="20%",      # percentage of parent width
+		height="20%",      # height in percentage of parent height
+		loc='upper right',
+		bbox_to_anchor=(0, 0, 1, 1),
+		bbox_transform=colorbar_ax.transAxes,
+		borderpad=1
+	)
+	
+	cbar = plt.colorbar(im, cax=cax, orientation='horizontal')
+	cbar.ax.xaxis.set_ticks_position('top')
+	
 	if Mlabel is not None:
-		cbar.set_label(Mlabel, labelpad=10)
+		cbar.set_label(Mlabel, labelpad=5)
+	
 		
 	# Set title, labels
 	if title is not None:
-		annotation_ax.set_title(title, pad=10)
+		annotation_ax.set_title(title, pad=10, size=11)
 	if xlabel is not None:
 		signal_ax.set_xlabel(xlabel)
 	if ylabel is not None:
 		signal_ax.set_ylabel(ylabel)
-		
-	annotation_ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-	legend_ax.axis("off")
-	plt.tight_layout()
+	
+	
+	# Making annotation axis spines thicker
+	if ann is not None:
+		annotation_ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+	else:
+		annotation_ax.axis("off")
+
+	fig.subplots_adjust(hspace=0.01, wspace=0.05)
 	plt.close()
 	return fig
