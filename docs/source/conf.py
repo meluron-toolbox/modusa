9b9bfcf0bc80907b45de0c1a9335f58bc43d91a0
Ankit Anand <ankit0.anand0@gmail.com>
2025-08-12 13:55:37 +0530

    [Bug] Fix minor error in distribution plotter

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'modusa'
 copyright = '2025, Ankit Anand'
 author = 'Ankit Anand'
-release = '0.3.25'
+release = '0.3.26'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
